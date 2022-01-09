eb8249667709eb54411c4c8d92f0a8214a56b443
Henrique Lino <hvlino007@gmail.com>
2022-01-09 14:13:18 -0300

    Loop Odd Numbers

--- 003_loop/index.js
+++ 003_loop/index.js
@@ -17,21 +17,20 @@
 // console.log(result);
 
  function addUpTo(num) {
-    let result = 0;
-	for (let i = 0; i <= num; i++) {
-        result = result + i;
-
+    let sum = 0;
+	for (let i = 1; i <= num; i = i + 2) {
+        sum = sum + i;
     }
-    return result;
+    return sum;
 }
-// console.log(addUpTo(3));
-// console.log(addUpTo(7));
-// console.log(addUpTo(10));
+console.log(addUpTo(3));
+console.log(addUpTo(7));
+console.log(addUpTo(89898));
 
 
 // Dado num, dividir n vezes por 10 até num ser menor que 1
 
-function sumDigits(num) {
+/* function sumDigits(num) {
     let n = num;
     let times = 0;
     while (n >= 1) {
@@ -43,6 +42,6 @@ function sumDigits(num) {
 
 console.log(sumDigits(99));
 console.log(sumDigits(10000));
-console.log(sumDigits(123245567));
+console.log(sumDigits(123245567)); */
 
 
